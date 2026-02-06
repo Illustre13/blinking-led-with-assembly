a805919bab70bb98ebc6e8491c8b1d881e05fc4d
Bertin NDAHAYO SINGIZWA <100227921+Illustre13@users.noreply.github.com>
2026-02-07 00:00:06 +0200

    changes

--- blink_assembler.X/blink_assembler.asm
+++ blink_assembler.X/blink_assembler.asm
@@ -1,71 +1,70 @@
 ;************************************************
-; Example 2
-; Description: Blinking LED using PIC Assembly (PIC-AS)
-; Author: Emmy (rewritten & cleaned)
+; Ex 2 Modified
+; Description: Blinking two LEDs alternately in Assembly language
+; Author: STUDENT
 ;************************************************
 
-        PROCESSOR 16F877A
-        #include <xc.inc>
+; Declaring constants
+ADCON1 equ 9fh ; BANK1 - analog-to-digital converter register
+STATUS equ 03h ; for selecting BANK
+RP0 equ 05h
+RP1 equ 06h
+PORTA equ 05h ; BANK0
+TRISA equ 85h ; BANK1
 
-;------------------------------------------------
-; Variable definitions (Bank 0 RAM)
-;------------------------------------------------
-COUNTER1    equ     0x20
-COUNTER2    equ     0x21
+COUNTER1 equ 20h ; first counter for delay loop
+COUNTER2 equ 21h ; second counter for delaying counter 1
 
-;------------------------------------------------
-; Reset Vector
-;------------------------------------------------
-        ORG     0x0000
-        GOTO    MAIN
+; Use directive to force the program to start at reset vector : 00h
+org 00h
+; Program starts here
+clrw ; clear W register
 
-;------------------------------------------------
-; Main Program
-;------------------------------------------------
-MAIN:
-        ; Clear W and PORTA
-        CLRW
-        CLRF    PORTA
+; Initialize PORTA
+BCF STATUS, RP0 ; RP0 =0
+BCF STATUS, RP1 ; RP1=0
+CLRF PORTA ; clear PORTA
 
-        ;----------------------------------------
-        ; Configure PORTA as digital outputs
-        ;----------------------------------------
-        BSF     STATUS, RP0        ; Bank 1
-        MOVLW   0x06               ; All PORTA pins digital
-        MOVWF   ADCON1
-        CLRF    TRISA              ; PORTA as output
-        BCF     STATUS, RP0        ; Back to Bank 0
+; Switch to Bank1 to configure ADCON1
+BSF STATUS, RP0 ; RP0 =1
+MOVLW 0x06 ; Configure all pins of PORTA as digital
+MOVWF ADCON1
 
-;------------------------------------------------
-; Main Loop
-;------------------------------------------------
+; Set PORTA as output
+MOVLW 0x00 ; clear W register
+MOVWF TRISA ; set PORTA pins as output
+
+; Back to Bank0 for working with PORTA
+BCF STATUS, RP0 ; RP0 =0
+BCF STATUS, RP1 ; RP1=0
+
+; Main loop
 START:
-        ; Turn ON LEDs (PORTA)
-        MOVLW   0xFF
-        MOVWF   PORTA
+    ; Turn ON RA0, turn OFF RA1
+    MOVLW 0x01 ; 0000 0001
+    MOVWF PORTA
+    ; Delay
+    MOVLW 0xFF
+    MOVWF COUNTER1
+    MOVWF COUNTER2
+DELAY1:
+    DECFSZ COUNTER1, 1
+    GOTO DELAY1
+    DECFSZ COUNTER2, 1
+    GOTO DELAY1
 
-        CALL    DELAY
+    ; Turn ON RA1, turn OFF RA0
+    MOVLW 0x02 ; 0000 0010
+    MOVWF PORTA
+    ; Delay
+    MOVLW 0xFF
+    MOVWF COUNTER1
+    MOVWF COUNTER2
+DELAY2:
+    DECFSZ COUNTER1, 1
+    GOTO DELAY2
+    DECFSZ COUNTER2, 1
+    GOTO DELAY2
 
-        ; Turn OFF LEDs (PORTA)
-        CLRF    PORTA
-
-        CALL    DELAY
-
-        GOTO    START
-
-;------------------------------------------------
-; Delay Subroutine
-;------------------------------------------------
-DELAY:
-        MOVLW   0xFF
-        MOVWF   COUNTER1
-        MOVWF   COUNTER2
-
-DELAY_LOOP:
-        DECFSZ  COUNTER1, F
-        GOTO    DELAY_LOOP
-        DECFSZ  COUNTER2, F
-        GOTO    DELAY_LOOP
-        RETURN
-
-        END
+GOTO START
+END
